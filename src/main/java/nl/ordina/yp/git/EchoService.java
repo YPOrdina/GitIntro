1178d7e86cb9642e510c5f785511abe603e479cf
Mustafa Cakir <a.m.cakir@gmail.com>
2014-09-18 12:03:00 +0200

    added extra parameter for indicating time of day

--- src/main/java/nl/ordina/yp/git/EchoService.java
+++ src/main/java/nl/ordina/yp/git/EchoService.java
@@ -5,4 +5,14 @@ public class EchoService {
     public String echo(String name) {
         return "hello, John!";
     }
+    
+    public String echo(String name, boolean isMorning) {
+		if (isMorning) {
+			return "Good morning, " + name + "!";
+		} else {
+			return "Good afternoon, " + name + "!";
+
+		}
+	}
+
 }
